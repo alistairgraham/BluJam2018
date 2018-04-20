288ceec1f020b07f0b3b8aa25ea0ee97cc3c37a3
Sam <samong0105@gmail.com>
2018-04-20 23:20:48 +1200

    Added Abstract class for Layer objects

--- src/main/java/Bombtastic.java
+++
@@ -1,9 +0,0 @@
-
-public class Bombtastic {
-
-	public Bombtastic() {
-		// TODO Auto-generated constructor stub
-		System.out.println("ehaeuhioash");
-	}
-
-}
---
+++ src/main/java/Layer.java
@@ -0,0 +1,13 @@
+/**
+ * Created by sam on 20/04/18.
+ */
+public abstract class Layer implements GameObject{
+    public abstract void draw();
+    public abstract void update();
+
+    public boolean contains(Asteroid a){
+        return false;
+    }
+
+
+}
