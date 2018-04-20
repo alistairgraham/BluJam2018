a9015c65548520e319dd8ad15f1f45d4672b9d38
Sam <samong0105@gmail.com>
2018-04-20 23:29:45 +1200

    Added layer objects

---
+++ src/main/java/CloudLayer.java
@@ -0,0 +1,14 @@
+/**
+ * Created by sam on 20/04/18.
+ */
+public class CloudLayer extends Layer {
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
---
+++ src/main/java/EarthLayer.java
@@ -0,0 +1,14 @@
+/**
+ * Created by sam on 20/04/18.
+ */
+public class EarthLayer extends Layer {
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
