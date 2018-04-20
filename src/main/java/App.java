fceef31cff26434f498920db3dee51f568a1381b
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2018-04-21 00:49:03 +1200

    Main Method ay

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,8 @@ public class App {
     }
 
     public static void main(String[] args) {
-        //System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
+    	// Use processing to set frame rate
         gameRunning = true;
         targetBlown = false;
         while (gameRunning) {
