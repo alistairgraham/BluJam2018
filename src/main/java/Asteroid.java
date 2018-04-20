fe5602cd437883a8fa0fad27818fac411d5f6134
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2018-04-20 23:40:57 +1200

    Asteroid: Get and setters

--- src/main/java/Asteroid.java
+++ src/main/java/Asteroid.java
@@ -7,7 +7,7 @@ public class Asteroid implements GameObject {
 	// PImage img;
 
 	public Asteroid(int x_pos, int y_pos) {
-		float velocity = -1;
+		float velocity = 1;
 		reset(x_pos, y_pos, velocity);
 	}
 	
@@ -24,4 +24,32 @@ public class Asteroid implements GameObject {
 		this.y = y_pos;
 		this.vel = velocity;
 	}
+	
+	//----------------------------------Getter and setters------------------------//
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getVel() {
+		return vel;
+	}
+
+	public void setVel(float vel) {
+		this.vel = vel;
+	}
+	//-------------------------------------------------------------------------------//
+	
+	
 }
