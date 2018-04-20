22cc38aa7fabd4ff54506ee59ca3343e665c122f
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2018-04-20 23:33:21 +1200

    Asteroid: Add reset method

--- src/main/java/Asteroid.java
+++ src/main/java/Asteroid.java
@@ -1,20 +1,27 @@
 
 public class Asteroid implements GameObject {
 	
-	private int x;
-	private int y;
+	private float x;
+	private float y;
+	private float vel;
 	// PImage img;
 
 	public Asteroid(int x_pos, int y_pos) {
-		this.x = x_pos;
-		this.y = y_pos;
+		float velocity = -1;
+		reset(x_pos, y_pos, velocity);
 	}
 	
 	public void update() {
-	
+		
 	}
 	
 	public void draw() {
 		
+	}
+	
+	public void reset(float x_pos, float y_pos, float velocity) {
+		this.x = x_pos;
+		this.y = y_pos;
+		this.vel = velocity;
 	}
 }
