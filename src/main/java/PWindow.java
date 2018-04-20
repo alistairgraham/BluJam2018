ff232eccebeb109cf23d0c0795e494176c1b0e66
Alex <alexjfmitchell@gmail.com>
2018-04-21 00:12:51 +1200

    Add initial rendering functionality

---
+++ src/main/java/PWindow.java
@@ -0,0 +1,36 @@
+import processing.core.PApplet;
+import util.Debug;
+
+import java.io.File;
+
+public class PWindow extends Renderer{
+
+    public PWindow(){
+
+    }
+
+    @Override
+    public void render(Scene scene) {
+
+    }
+
+    /**
+     * Load the assets within the system
+     * @param path Path to directory
+     */
+    private void loadAssets(String path){
+        File folder = new File(path);
+        File[] folderItems = folder.listFiles();
+
+        for (File folderItem : folderItems) {
+            if (folderItem.isFile()) {
+                Debug.log("Loaded File: " + folderItem.getName() + " As: " + folderItem.getName().split("\\.")[0]);
+//                AssetHandler.loadImage(root + "/" + listOfFiles[i].getName(), listOfFiles[i].getName().split("\\.")[0], this);
+            } else if (folderItem.isDirectory()) {
+                Debug.log("Directory " + folderItem.getName());
+                loadAssets(path + "/" + folderItem.getName());
+            }
+        }
+
+    }
+}
