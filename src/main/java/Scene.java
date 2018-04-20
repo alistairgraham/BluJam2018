6f99cbd4c3219ca3e4d00ed927654694dd26bb0d
Sam <samong0105@gmail.com>
2018-04-21 00:28:02 +1200

    Deleted every layer

--- src/main/java/Scene.java
+++ src/main/java/Scene.java
@@ -7,11 +7,6 @@ import gameObjects.layers.*;
 public class Scene {
     public Scene(){
         Asteroid a = new Asteroid();
-        SpaceLayer spaceLayer = new SpaceLayer();
-        CloudLayer cloudLayer = new CloudLayer();
-        WindLayer windLayer = new WindLayer();
-        EmptyLayer emptyLayer = new EmptyLayer();
-        EarthLayer earthLayer = new EarthLayer();
 
     }
 }
