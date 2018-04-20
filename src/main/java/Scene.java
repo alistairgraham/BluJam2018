52c23c1e7be8f7a9253393ad4cf8edf85ea33e44
Sam <samong0105@gmail.com>
2018-04-21 00:33:09 +1200

    Remade every layer & made a set of layers

--- src/main/java/Scene.java
+++ src/main/java/Scene.java
@@ -1,17 +1,33 @@
 import gameObjects.Asteroid;
 import gameObjects.layers.*;
 
+import java.util.HashSet;
+
 /**
  * Created by sam on 20/04/18.
  */
 public class Scene {
+    HashSet<Layer> layers;
+    Asteroid asteroid;
     public Scene(){
-        Asteroid a = new Asteroid();
-        SpaceLayer spaceLayer = new SpaceLayer();
-        CloudLayer cloudLayer = new CloudLayer();
-        WindLayer windLayer = new WindLayer();
-        EmptyLayer emptyLayer = new EmptyLayer();
-        EarthLayer earthLayer = new EarthLayer();
-
+        asteroid = new Asteroid();
+        layers = new HashSet<>();
+        layers.add(new SpaceLayer());
+        layers.add(new CloudLayer());
+        layers.add(new WindLayer());
+        layers.add(new EmptyLayer());
+        layers.add(new EarthLayer());
     }
+
+    public Asteroid getAsteroid() {
+        return asteroid;
+    }
+
+    public HashSet<Layer> getLayers(){
+        return layers;
+    }
+
+
+
+
 }
