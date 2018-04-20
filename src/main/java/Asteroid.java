a22a04562ea2442ed3eb8ade8aa0762401da6580
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2018-04-20 23:17:46 +1200

    Added the Asteroid Class outline

---
+++ src/main/java/Asteroid.java
@@ -0,0 +1,20 @@
+
+public class Asteroid implements GameObject {
+	
+	private int x;
+	private int y;
+	// PImage img;
+
+	public Asteroid(int x_pos, int y_pos) {
+		this.x = x_pos;
+		this.y = y_pos;
+	}
+	
+	public void update() {
+	
+	}
+	
+	public void draw() {
+		
+	}
+}
