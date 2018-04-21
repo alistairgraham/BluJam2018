8d3da2d3f5891c8fb86706d501a7f7ded684d3f0
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2018-04-22 02:05:12 +1200

    Added user movement

--- src/main/java/Asteroid.java
+++ src/main/java/Asteroid.java
@@ -8,13 +8,15 @@ public class Asteroid implements Drawable {
 
 	private PVector position;
 	private PVector velocity;
+	private PVector velX;
 	private float diam;
 
 	public Asteroid(float xPos, float yPos, float diameter) {
 		initialPosition = new PVector(xPos, yPos);
 		position = new PVector(xPos, yPos);
-		initialVelocity = new PVector(0,1/2);
+		initialVelocity = new PVector(0,2);
 		velocity = new PVector(0, 2); // Default velocity vector
+        velX = new PVector(-0.2f, 0);
 //		position = initialPosition.copy();
 //		initialVelocity = new PVector(0,2);
 //		velocity = initialVelocity.copy();
@@ -25,12 +27,13 @@ public class Asteroid implements Drawable {
 	public void draw(PApplet pApplet) {
         PVector rV = new PVector(-velocity.x, -velocity.y).mult(10);
         PVector pV = new PVector(velocity.y, -velocity.x).normalize().mult(diam/2);
+
         pApplet.fill(pApplet.color(249, 181, 59));
         PVector p1 = rV.add(new PVector(rV.x, rV.y).normalize().mult(20)).add(position);
-
         pApplet.triangle(p1.x, p1.y,
                 position.x+pV.x, position.y+pV.y,
                 position.x-pV.x, position.y-pV.y);
+
 		pApplet.fill(pApplet.color(112, 21, 13));
 		pApplet.ellipse(position.x, position.y, diam, diam);
 
@@ -44,13 +47,30 @@ public class Asteroid implements Drawable {
 		return position.y;
 	}
 
-	public void update(App app){
+	public void update(App app, float dx){
 	    velocity.add(0, 1/10); // Temporary value for velocity
 		position.add(velocity);
 		if(position.x > app.width-1) position.x = 1;
 		if(position.x<=0) position.x = app.width-1;
 		//position.x = Math.min(app.width - 1, Math.max(0, position.x));
 	}
+
+	public void userMove(boolean left){
+	    if (left) {
+            if (velocity.x>-6) {
+                velocity.add(velX);
+                if (velocity.x<-6) { velocity.x = -6; }
+            }
+            position.add(velX);
+        }
+        else {
+            if (velocity.x<6) {
+                velocity.add(velX.mult(-1));
+                if (velocity.x>6) { velocity.x = 6; }
+            }
+            position.add(velX.mult(-1));
+        }
+    }
 
 	public PVector getPosition() {
 		return position;
