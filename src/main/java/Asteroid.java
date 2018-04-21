bd4b619dcfa4011a4a314f51d6d8d510878f7b4b
Alex <alexjfmitchell@gmail.com>
2018-04-22 01:29:09 +1200

    Add gravity to the asteroid

--- src/main/java/Asteroid.java
+++ src/main/java/Asteroid.java
@@ -3,73 +3,76 @@ import processing.core.PVector;
 
 public class Asteroid implements Drawable {
 
-	private PVector initialPosition;
-	private PVector initialVelocity;
+    private PVector initialPosition;
+    private PVector initialVelocity;
 
-	private PVector position;
-	private PVector velocity;
-	private float diam;
+    private PVector position;
+    private PVector velocity;
+    private float diam;
 
-	public Asteroid(float xPos, float yPos, float diameter) {
-		initialPosition = new PVector(xPos, yPos);
-		position = new PVector(xPos, yPos);
-		initialVelocity = new PVector(0,1/2);
-		velocity = new PVector(0, 2); // Default velocity vector
+    public static final PVector gravity = new PVector(0f, 0.01f);
+
+    public Asteroid(float xPos, float yPos, float diameter) {
+        initialPosition = new PVector(xPos, yPos);
+        position = new PVector(xPos, yPos);
+        initialVelocity = new PVector(0, 1 / 2);
+        velocity = new PVector(0, 2); // Default velocity vector
 //		position = initialPosition.copy();
 //		initialVelocity = new PVector(0,2);
 //		velocity = initialVelocity.copy();
-		this.diam = diameter;
-	}
+        this.diam = diameter;
+    }
 
-	@Override
-	public void draw(PApplet pApplet) {
+    @Override
+    public void draw(PApplet pApplet) {
         PVector rV = new PVector(-velocity.x, -velocity.y).mult(10);
-        PVector pV = new PVector(velocity.y, -velocity.x).normalize().mult(diam/2);
+        PVector pV = new PVector(velocity.y, -velocity.x).normalize().mult(diam / 2);
         pApplet.fill(pApplet.color(249, 181, 59));
         PVector p1 = rV.add(new PVector(rV.x, rV.y).normalize().mult(20)).add(position);
 
         pApplet.triangle(p1.x, p1.y,
-                position.x+pV.x, position.y+pV.y,
-                position.x-pV.x, position.y-pV.y);
-		pApplet.fill(pApplet.color(112, 21, 13));
-		pApplet.ellipse(position.x, position.y, diam, diam);
+                position.x + pV.x, position.y + pV.y,
+                position.x - pV.x, position.y - pV.y);
+        pApplet.fill(pApplet.color(112, 21, 13));
+        pApplet.ellipse(position.x, position.y, diam, diam);
 
-	}
+    }
 
-	public float getX() {
-		return position.x;
-	}
+    public float getX() {
+        return position.x;
+    }
 
-	public float getY() {
-		return position.y;
-	}
+    public float getY() {
+        return position.y;
+    }
 
-	public void update(App app){
-	    velocity.add(0, 1/10); // Temporary value for velocity
-		position.add(velocity);
-		if(position.x > app.width-1) position.x = 1;
-		if(position.x<=0) position.x = app.width-1;
-		//position.x = Math.min(app.width - 1, Math.max(0, position.x));
-	}
+    public void update(App app) {
+        velocity.add(0, 1 / 10); // Temporary value for velocity
+        position.add(velocity);
+        velocity.add(gravity);
+        if (position.x > app.width - 1) position.x = 1;
+        if (position.x <= 0) position.x = app.width - 1;
+        //position.x = Math.min(app.width - 1, Math.max(0, position.x));
+    }
 
-	public PVector getPosition() {
-		return position;
-	}
+    public PVector getPosition() {
+        return position;
+    }
 
-	public void setPosition(PVector position) {
-		this.position = position;
-	}
+    public void setPosition(PVector position) {
+        this.position = position;
+    }
 
-	public PVector getVelocity() {
-		return velocity;
-	}
+    public PVector getVelocity() {
+        return velocity;
+    }
 
-	public void setVelocity(PVector velocity) {
-		this.velocity = velocity;
-	}
+    public void setVelocity(PVector velocity) {
+        this.velocity = velocity;
+    }
 
-	public void reset(){
-		position = initialPosition.copy();
-		velocity = initialVelocity.copy();
-	}
+    public void reset() {
+        position = initialPosition.copy();
+        velocity = initialVelocity.copy();
+    }
 }
